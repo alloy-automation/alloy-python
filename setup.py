a056960822f9fbfe561facbc712141017cb1c383
Kelly Gold <kellygold@Kellys-MacBook-Pro-2.local>
2023-12-07 15:46:00 -0700

    setup updates

--- setup.py
+++ setup.py
@@ -7,9 +7,19 @@ setup(
     author='Your Name',
     author_email='kelly@runalloy.com',
     description='A lightweight Python wrapper for the Alloy API',
+    long_description=open('README.md').read(),  # Ensure you have a README.md file
+    long_description_content_type='text/markdown',
+    url='https://github.com/alloy-automation/alloy-python',  # Replace with your repository URL
     packages=find_packages(),
     install_requires=[
-        'requests',  # and any other dependencies
+        'requests',  # Add other dependencies as needed
     ],
-    # Include additional metadata as needed
+    classifiers=[
+        # Classifiers help users find your project
+        # Full list: https://pypi.org/classifiers/
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Update if you have a different license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the minimum Python version required
 )
